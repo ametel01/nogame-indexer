3735ede41cae72e1ae52da45e1c73df87149bf21
ametel01 <alex-metelli@gmx.com>
2024-01-11 14:22:32 +0800

    fixes

--- src/resourcesSpent.ts
+++ src/resourcesSpent.ts
@@ -1,17 +1,16 @@
-// import { hash } from "https://esm.sh/starknet@5.14";
-import { Block, EventWithTransaction } from "./common/deps.ts";
+import { Block, EventWithTransaction } from './common/deps.ts';
 import {
   SELECTOR_KEYS,
   NOGAME_CONTRACT,
   STARTING_BLOCK,
   SEPOLIA_URL,
   formatFelt,
-} from "./common/constants.ts";
+} from './common/constants.ts';
 
 export const config = {
   streamUrl: SEPOLIA_URL,
   startingBlock: STARTING_BLOCK,
-  network: "starknet",
+  network: 'starknet',
   filter: {
     header: { weak: true },
     events: [
@@ -33,18 +32,18 @@ export const config = {
       },
     ],
   },
-  sinkType: "postgres",
+  sinkType: 'postgres',
   sinkOptions: {
-    connectionString: Deno.env.get("PGQL_CONNECTION"),
-    tableName: "resourcespent",
-    tlsCertificate: Deno.env.get("PEM_CERTIFICATE"),
+    connectionString: Deno.env.get('PGQL_CONNECTION'),
+    tableName: 'resourcespent',
+    tlsCertificate: Deno.env.get('PEM_CERTIFICATE'),
     tlsAcceptInvalidCertificates: true,
   },
 };
 
 export default function transform({ events, header }: Block) {
   if (!header) {
-    console.log("missing header, unable to process", events.length, "events");
+    console.log('missing header, unable to process', events.length, 'events');
     return;
   }
 
@@ -56,21 +55,22 @@ export default function transform({ events, header }: Block) {
 
     // Temporary to avoid null pk error
     const uniqueId =
-      Math.floor(Date.now() / 1000) + Math.floor(Math.random() * 1000);
+      Math.floor(Date.now() / 1000) +
+      (Math.floor(Math.random() * 1000) % 2147483647);
 
-    let type = "";
+    let type = '';
     switch (key) {
       case SELECTOR_KEYS.COMPOUNDS:
-        type = "compound";
+        type = 'compound';
         break;
       case SELECTOR_KEYS.TECHS:
-        type = "tech";
+        type = 'tech';
         break;
       case SELECTOR_KEYS.FLEET:
-        type = "fleet";
+        type = 'fleet';
         break;
       case SELECTOR_KEYS.DEFENCES:
-        type = "defence";
+        type = 'defence';
         break;
       default:
         return;
